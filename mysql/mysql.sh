9f6eb172651d972560b704c658f4e017b9bf9f55
Steven Sun <yunlin.sun@liferay.com>
2017-08-25 14:48:11 +0800

    Update mysql.sh info

--- mysql/mysql.sh
+++ mysql/mysql.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+# Add the following to your /etc/mysql/my.cnf, So you don't need to write password in the script
+# [client]
+# password=****
+
 
 USER="root"
 dblist="test test1 master ee70x ee6210 ee62x"
