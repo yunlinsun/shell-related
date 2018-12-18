775e43749fc56c516e8b6b1ce9503a5ce9418255
Steven Sun <yunlin.sun@liferay.com>
2018-12-18 15:17:07 +0800

    update gauntlet.sh

--- gauntlet-prepared/gauntlet.sh
+++ gauntlet-prepared/gauntlet.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 downloadpath=/home/steven/Downloads
-bundle_zip=liferay-portal-bundle-tomcat.zip
+bundle_zip=7.1.x_Gauntlet_2018-dec-13_bucket-a_private.tar.gz
 workspace=/home/steven/project/bundles
 unzip_folder=bundles
 
@@ -8,38 +8,31 @@ prepare(){
 	echo "Go to workspace..."
 	cd $workspace
 
-	if [[ -d "$bundle_zip" ]]; then
-		echo "There is $bundle_zip already, will delete it..."
-		rm -rf bundle_zip
-		echo "Move $bundle_zip to $workspace..."
-		mv $downloadpath/$bundle_zip $workspace/$bundle_zip
-	else
-		echo "Move $bundle_zip to $workspace..."
-		mv $downloadpath/$bundle_zip $workspace/$bundle_zip
-	fi
-
 	if [[ -d "$unzip_folder" ]]; then
 		echo "There is $unzip_folder already, will delete it..."
 		rm -rf $unzip_folder
-		echo "Unzip $bundle_zip"
-		unzip -q ${bundle_zip}
+		tar -xvf $downloadpath/$bundle_zip -C $workspace
+		 
 	else
-		echo "Unzip $bundle_zip"
-		unzip -q ${bundle_zip}
-	fi
+		echo "Unzip $bundle_zip" to $workspace
+		tar -xvf $downloadpath/$bundle_zip -C $workspace
+	fi	
 	echo "[INFO] Please Enter the Branch name of your bundle..."
 	echo "[INFO] E.g: 7.0.x_Gauntlet_2018-feb-02_bucket-b_private"
 	echo -n "$PS3"
 	read branchname
+	echo "Move $bundle_zip to ${branchname}.zip..."
+	mv $downloadpath/$bundle_zip "$downloadpath/${branchname}.zip"
 	echo "Move $unzip_folder to $branchname..."
 	mv $unzip_folder "$branchname"
 	echo "Copy portal-ext.properties to tomcat folder..."
 	cp -r $workspace/portal-ext.properties $workspace/$branchname
-	echo "Replace setenv.sh..."
-	cp -r $workspace/setenv.sh $workspace/$branchname/tomcat-8.0.32/bin
+	echo "Copy setenv.sh to tomcat folder..."
+	cp -r $workspace/setenv.sh $workspace/$branchname/tomcat*/bin
 
-	echo "Clear Database"
-	sh ~/mysql.sh
+	# echo "Clear Database"
+	# sh ~/mysql.sh
+	echo "PWD: $workspace/$branchname"
 }
 
 prepare
