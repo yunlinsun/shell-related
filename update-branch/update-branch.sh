5ea99f6e117904230da34e08e864117fb24ca7fe
Steven Sun <yunlin.sun@liferay.com>
2019-01-10 09:02:03 +0800

    update set--upstream

--- update-branch/update-branch.sh
+++ update-branch/update-branch.sh
@@ -13,7 +13,7 @@ do
 	git clean -fd
 	git checkout -f
 	git merge --ff-only upstream/$branchname
-	# git push origin $branchname
+	git push --set-upstream origin $branchname
 done
 
 echo -e "[Info] The running time of script is $SECONDS seconds ...\n"
@@ -21,11 +21,11 @@ echo -e "[Info] The running time of script is $SECONDS seconds ...\n"
 
 set -x
 
-branchlist="7.0.x 7.1.x master"
+branchlist="7.0.x 7.1.x 7.2.x master"
 sourcedir="/home/steven/project/liferay-portal"
 update
 
-branchlist="master-private 7.1.x-private 7.0.x-private ee-6.2.x ee-6.2.10 master 7.1.x 7.0.x"
+branchlist="master-private 7.1.x-private 7.0.x-private ee-6.2.x ee-6.2.10 7.0.x master 7.2.x 7.1.x"
 sourcedir="/home/steven/project/liferay-portal-ee"
 update 
 
