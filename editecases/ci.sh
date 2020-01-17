4c515acd6b8caa0b7815db26da9868d33ccb6c42
Steven Sun <yunlin.sun@liferay.com>
2020-01-17 16:08:27 +0800

    LRQA-55084 Update shell script to match new test.properties

--- editecases/ci.sh
+++ editecases/ci.sh
@@ -2,7 +2,7 @@
 
 ###### For linux User ########
 Casesfromsheet="/home/steven/project/script/poshi-script/Casesfromsheet.txt" # Creat Casesfromsheet.txt manually, copy & paste value from google sheet.
-portalweb_dir="/home/steven/project/liferay-portal-ee/portal-web" # Your portal-web dir ## Example for windows: portalweb_dir="/d/Git/portal/70/portal-web"
+portalweb_dir="/home/steven/project/liferay-portal/portal-web" # Your portal-web dir ## Example for windows: portalweb_dir="/d/Git/portal/70/portal-web"
 portal_acceptance="stevensun" # Your value
 #############################
 ## Do not edit the following
@@ -17,21 +17,28 @@ do
 	casename=`echo $line|cut -f1 -d "#"`
 	macroname=`echo $line|cut -f2 -d "#"`
 	file=$(echo $casename.testcase | xargs find $portalweb_dir -iname) 
-	sed "/test $macroname /a\        property portal.acceptance \= \"$portal_acceptance\"\; " -i $file
+	sed "/test $macroname /a\                property portal.acceptance \= \"$portal_acceptance\"\; " -i $file
 done < $Casesfromsheet
 
 set -v
 
-LINE_NUM1=$(sed -n '/    test.batch.names\=/=' $test_properties)
+LINE_NUM1=$(sed -n '/    test.batch.names\[acceptance-ce\]\=/=' $test_properties)
 LINE_NUM2=$(sed -n '/    wsdd-builder-jdk8/=' $test_properties)
-sed -i "${LINE_NUM1},${LINE_NUM2}c \    test.batch.names\=functional-tomcat90-mysql57-jdk8" $test_properties
 
-LINE_NUM3=$(sed -n '/test.batch.run.property.query\[functional-tomcat90-mysql57-jdk8\]\=/=' $test_properties)
-sed -i "${LINE_NUM3}s/^.*$/    test.batch.run.property.query\[functional-tomcat90-mysql57-jdk8\]\=portal.acceptance\ \=\=\ $portal_acceptance/" $test_properties
+sed -i "${LINE_NUM1},${LINE_NUM2}d" $test_properties
+
+LINE_NUM3=$(sed -n '/    test.batch.names\[acceptance-dxp\]\=/=' $test_properties)
+LINE_NUM4=$(grep -n -m 1 "#modules-integration-sybase160-jdk8" $test_properties |sed  's/\([0-9]*\).*/\1/')
+
+
+sed -i "${LINE_NUM3},${LINE_NUM4}c \    test.batch.names\[acceptance-dxp\]\=functional-tomcat90-mysql57-jdk8" $test_properties
+
+LINE_NUM5=$(sed -n '/test.batch.run.property.query\[functional-tomcat90-mysql57-jdk8\]\=/=' $test_properties)
+sed -i "${LINE_NUM5}s/^.*$/    test.batch.run.property.query\[functional-tomcat90-mysql57-jdk8\]\=portal.acceptance\ \=\=\ $portal_acceptance/" $test_properties
 sed -i '/test.batch.dist.app.servers=/,+6d' $test_properties
 
 
-cd $portalweb_dir/../modules
-../gradlew -b util.gradle formatSourceLocalChanges
+# cd $portalweb_dir/../modules
+# ../gradlew -b util.gradle formatSourceLocalChanges
 
 echo "The script has ran $SECONDS seconds."
