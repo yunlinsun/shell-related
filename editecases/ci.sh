9d18243bbfcfdde305ff3e82afcdacaf52ea73fc
Steven Sun <yunlin.sun@liferay.com>
2021-03-17 17:17:37 +0800

    LRQA-65878 Update var to match 7.1.x

--- editecases/ci.sh
+++ editecases/ci.sh
@@ -2,7 +2,7 @@
 
 ###### Edit to your own ########
 Casesfromsheet="/home/steven/project/script/poshi-script/Casesfromsheet.txt" # Creat Casesfromsheet.txt manually, copy & paste value from google sheet.
-portal_dir="/home/steven/project/liferay-portal-ee-7.3.x" # Your portal dir ## Example for windows: portal_dir="/d/liferay-portal"
+portal_dir="/home/steven/project/liferay-portal-ee-7.2.x" # Your portal dir ## Example for windows: portal_dir="/d/liferay-portal"
 portal_acceptance="stevensun" # Your value
 #############################
 ## Do not edit the following
@@ -43,7 +43,7 @@ LINE_NUM4=$(grep -n " wsdd-builder-jdk8" $test_properties | cut -f1 -d:)
 sed -i "${LINE_NUM3},${LINE_NUM4}d" $test_properties
 
 LINE_NUM5=$(grep -n "test.batch.names\[acceptance-dxp\]\=" $test_properties | cut -f1 -d:)
-LINE_NUM6=$(grep -n " functional-tomcat90-sqlserver2019-jdk8" $test_properties | cut -f1 -d:)
+LINE_NUM6=$($(grep -n " functional-tomcat90-sqlserver2019-jdk8" $test_properties | cut -f1 -d:)+1)
 # LINE_NUM4=$(grep -n -m 1 "#modules-integration-sybase160-jdk8" $test_properties |sed  's/\([0-9]*\).*/\1/')
 sed -i "${LINE_NUM5},${LINE_NUM6}c \    test.batch.names\[acceptance-dxp\]\=functional-tomcat90-mysql57-jdk8" $test_properties
 
